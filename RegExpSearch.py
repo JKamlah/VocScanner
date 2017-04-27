14232cd13bddba805f4ef6ee31b5265574bcaab5
jkamlah <jkamlah@ad.uni-mannheim.de>
2017-04-27 15:19:40 +0200

    Added some new features and commens

--- RegExpSearch.py
+++ RegExpSearch.py
@@ -14,11 +14,18 @@ import re
 # Iterate over the years
 def main(argv=None):
     # Init
-    dict_typ = 'Forenames'
+    # Dict = normal dictonary
+    # Cities = dictionary contains only Cities
+    # Forenames = dictionary contains only Forenames
+    dict_typ = 'Dict'
     dict_lng = 'ger'
     wordfreq = 15
     year_start = 1956
     year_end = 1957
+    word_min_len = 3
+    # Status True print everything that is found in the dict
+    # Status False print everything that isnt found in the dict
+    Status = True
     for i in range(year_start, year_end):
         # Read the dictonary File and split it into a list
         dict_file = open("Dictionaries\\%s_%s.txt" % (dict_typ,dict_lng), 'r')
@@ -28,17 +35,23 @@ def main(argv=None):
         # Read the datafile
         txt_file = open("U:\\Eigene Dokumente\\Aktienfuehrer_Dokumente\\Aktienfuehrer_PostOCR_Format\\%d\\WordFreq_%d" % (i,i), 'r')
         # Open outputfile
-        output_file = open("U:\\Eigene Dokumente\\Aktienfuehrer_Dokumente\\Aktienfuehrer_PostOCR_Format\\%d\\%sdd_%d.txt" % (i,dict_typ,i), 'w')
+        output_file = open("U:\\Eigene Dokumente\\Aktienfuehrer_Dokumente\\Aktienfuehrer_PostOCR_Format\\%d\\%s_%d.txt" % (i,dict_typ,i), 'w')
         # Search in every line of the datafile the given words of the dictonary
         for line in txt_file:
             if (int(re.search(r"\d*", line).group(0)) > wordfreq):
                 count = 0;
                 for word in dict_list:
-                    if len(word) > 3:
-                        if line.find(word) != -1:
-                            output_file.write(word)
-                            del dict_list[count]
-                            break
+                    if len(word) > word_min_len:
+                        if (Status == True):
+                                if line.find(word) != -1:
+                                    output_file.write(word)
+                                    del dict_list[count]
+                                    break
+                        if (Status == False):
+                                if line.find(word) != -1:
+                                    break
+                                if word == dict_list[-1]:
+                                    output_file.write(re.search(r"\s*", line).group(0))
                     count +=1;
         output_file.close()
         dict_file.close()
